09a8fe8a2c1cf4fba0b2d20576a03de0c087f27e
Radik Zabirov <zabirovradik070@gmail.com>
2024-04-12 12:41:49 +0400

    now the user can use their own json

--- lab_3/main.py
+++ lab_3/main.py
@@ -12,46 +12,44 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 if __name__ == "__main__":
-    try:
-        with open(os.path.join("lab_3", "json", "settings.json"),
-                    mode = "r",
-                    encoding = "utf-8") as f:
-                file = json.load(f)
-    except Exception as e:
-         logging.exception(e)
     parser = argparse.ArgumentParser()
     parser.add_argument('-m',
                        '--mode',
                        type= int,
                        default= Mode.DECRYPT,
                        help= 'Выбор режима работы')
+    parser.add_argument('-j',
+                       '--json_file_path',
+                       type= str,
+                       default= os.path.join("lab_3", "json", "settings.json"),
+                       help= 'Путь к файлу json для пользовательских настроек')
     args = parser.parse_args()
-    cryptography = Cryptograthy(os.path.join(file["main_folder"],
-                                             file["keys"],
+    try:
+        with open(args.json_file_path,
+                    mode = "r",
+                    encoding = "utf-8") as f:
+                file = json.load(f)
+    except Exception as e:
+         logging.exception(e)
+    cryptography = Cryptograthy(os.path.join(file["path_keys"],
                                              file["symmetric_key"]),
-                                os.path.join(file["main_folder"],
-                                             file["keys"],
+                                os.path.join(file["path_keys"],
                                              file["public_key"]),
-                                os.path.join(file["main_folder"],
-                                             file["keys"],
+                                os.path.join(file["path_keys"],
                                              file["secret_key"])
                                 )
     match args.mode:
         case Mode.GENERATE:
             cryptography.generate_and_save_keys(file["key_size"])
         case Mode.ENCRYPT:
-              cryptography.encript_by_hybrid(os.path.join(file["main_folder"],
-                                                          file["files"],
+              cryptography.encript_by_hybrid(os.path.join(file["files"],
                                                           file["initial_file"]),
-                                            os.path.join(file["main_folder"],
-                                                         file["files"],
+                                            os.path.join(file["files"],
                                                          file["encrypted_file"])
                                             )
         case Mode.DECRYPT:
-               cryptography.decript_by_hybrid(os.path.join(file["main_folder"],
-                                                           file["files"],
+               cryptography.decript_by_hybrid(os.path.join(file["files"],
                                                            file["encrypted_file"]),
-                                              os.path.join(file["main_folder"],
-                                                           file["files"],
+                                              os.path.join(file["files"],
                                                            file["decrypted_file"])
                                             )
